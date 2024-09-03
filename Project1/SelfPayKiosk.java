07ca838292e5f694550ebed3965f45e32eef7d4f
Cieran Wong <95898976+CieranW@users.noreply.github.com>
2024-09-02 18:19:49 -0700

    Completed Steps 1 - 7 in SelfPayKiosk

--- Project1/SelfPayKiosk.java
+++ Project1/SelfPayKiosk.java
@@ -1,37 +1,45 @@
 public class SelfPayKiosk{
    private final double SALES_TAX = 0.07;
+   private int numCustomers;
+   private double totalSales;
+   private double amountDue;
 
    // Constructor
    public SelfPayKiosk(){
       /* Complete the method */ 
+      numCustomers = 0;
+      totalSales = 0.0;
+      amountDue = 0.0;
    } 
 
    // Return total daily sales    
    public double getTotalSales() {
       /* Update the return statment */        
-      return -1.0;
+      return totalSales;
    }
 
    // Return current amount due     
    public double getAmountDue() {
       /* Update the return statment */        
-      return -1.0;
+      return amountDue;
    }
 
    // Return number of customers served     
    public int getNumCustomers() {
       /* Update the return statment */        
-      return -1;
+      return numCustomers;
    } 
 
    // Scan one item  
    public void scanItem(double price) {   
       /* Complete the method */ 
+      amountDue += price;
    }
 
    // Apply sales tax to current purchases
    public void checkOut() {
       /* Complete the method */ 
+      amountDue += (amountDue * SALES_TAX);
    }
 
    // Cancel current purchases    
@@ -42,15 +50,32 @@ public class SelfPayKiosk{
    // Reset register for the day    
    public void resetKiosk() {      
       /* Complete the method */ 
+      numCustomers = 0;
+      totalSales = 0.0;
+      amountDue = 0.0;
    }  
 
    // Apply payment to amount due 
    public void makePayment(double payment) {
       /* Complete the method */ 
+      if (payment >= amountDue) {
+         totalSales += amountDue;
+         numCustomers++;
+         amountDue = 0.0;
+      } else {
+         totalSales += payment;
+         amountDue -= payment;
+      }
    }
 
    // Simulate multiple transactions
    public void simulateSales(int numSales, double initialPrice, double incrPrice) {
       /* Complete the method */ 
+      for (int i = 0; i < numSales; i++) {
+         scanItem(initialPrice);
+         checkOut();
+         makePayment(initialPrice + 1.0);
+         initialPrice += incrPrice;
+      }
    }
 }
