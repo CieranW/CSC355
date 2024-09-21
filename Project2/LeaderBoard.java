c8303ddd47d745a5c26106c2adbc2377bf67f12a
Cieran Wong <95898976+CieranW@users.noreply.github.com>
2024-09-20 22:08:56 -0700

    Completed

--- Project2/LeaderBoard.java
+++ Project2/LeaderBoard.java
@@ -16,7 +16,31 @@ public class LeaderBoard {
     }
 
     Contestant[] finalBoard() {
-        for (int i = 0; i < contestants.length; i++) {
+        int n = array.length;
+        boolean swapped;
+
+        // Outer loop for each pass
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            // Inner loop for comparing and swapping elements
+            for (int j = 0; j < n - i - 1; j++) {
+                // Compare adjacent elements and swap if needed (Descending order)
+                if (array[j] < array[j + 1]) {
+                    // Swap the elements
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    // Mark swapped as true
+                    swapped = true;
+                }
+            }
+
+            // If no swaps were made, the array is already sorted
+            if (!swapped) {
+                break;
+            }        for (int i = 0; i < contestants.length; i++) {
             for (int j = 0; j < contestants.length; j++) {
                 if (contestants[j].contestantScore > contestants[i].contestantScore) {
                     Contestant temp = contestants[i];
@@ -26,6 +50,6 @@ public class LeaderBoard {
             }
         }
         return contestants;
+    }    
     }
-    
 }
