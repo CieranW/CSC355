aaf0d390f66af133b0bcbdec1f4decf417766351
CieranW <cieranwong@gmail.com>
2024-10-29 10:19:20 -0700

    Changed main file name cause my computer was having a fit

--- SearchAlgorithms/ToRun.java
+++ SearchAlgorithms/ToRun.java
@@ -1,4 +1,4 @@
-public class main {
+public class ToRun {
     public static int findMinVal(int [] arr) {
         int minVal = arr[0];
         for (int i = 1; i < arr.length; ++i) {
@@ -26,7 +26,41 @@ public class main {
         }
     }
 
-    public static void Main(String[] args) {
+    public static int binarySearch(int [] numbers, int key) {
+      int mid;
+      int low;
+      int high;
+
+      low = 0;
+      high = numbers.length - 1;
+
+      while (high >= low) {
+         mid = (high + low) / 2;
+         if (numbers[mid] < key) {
+            low = mid + 1;
+         } 
+         else if (numbers[mid] > key) {
+            high = mid - 1;
+         } 
+         else {
+            return mid;
+         }
+      }
+      return -1; // not found
+   }
+
+   public static int linearSearch(int [] numbers, int key) {
+      int i;
+
+      for (i = 0; i < numbers.length; ++i) {
+         if (numbers[i] == key) {
+            return i;
+         }
+      }
+      return -1; /* not found */
+   }
+
+    public static void main(String[] args) {
         int [] arr1 = {100, 775, 146, 483, 789, 990, 844, 926, 89, 416, 540, 747, 398, 608, 356, 84, 315, 971, 834, 591, 447};
         int [] arr2 = {915, 550, 369, 666, 100, 435, 638, 342, 440, 780, 902, 901, 329, 115, 29, 361, 930, 505, 439, 75, 322};
 
